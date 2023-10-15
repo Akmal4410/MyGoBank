e73b81c599a0934dea88583f90e669f319b1bce0
Akmakl4410 <akmalmahmoodkinan@gmail.com>
2023-10-15 09:14:01 +0530

    trasnfer doing

--- types.go
+++ types.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+type TransferRequest struct {
+	ToAccound int64 `json:"toAccount"`
+}
+
 type CreateAccountRequest struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
